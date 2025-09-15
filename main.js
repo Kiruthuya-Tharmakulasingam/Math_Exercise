578d685bac2f249777eef4670f5b2a8fd141e642
Kiruthuya <kiruthuyakiruthu@gmail.com>
2025-09-15 22:02:39 +0530

    validation and calculation

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-function validateInput() {
-      const num = document.getElementById("number").value;
-      const result = document.getElementById("result");
+const input = document.getElementById("number");
+const button = document.getElementById("calcBtn");
+const resultbox = document.getElementById("result");
 
-      // If invalid → show error
-      if (num === "" || isNaN(num) || Number(num) < 0) {
-        result.textContent = "Please enter a valid number!";
-      } else {
-        result.textContent = ""; // If valid → show nothing
-      }
-    }
+button.addEventListener("click", function () {
+  const num = input.value;
+
+  //validation and calculation
+  if (num === "" || isNaN(num) || Number(num) < 0) {
+    resultbox.textContent = "Please enter a valid number!";
+    resultbox.style.color = "red";
+  } else {
+    const value = Number(num);
+    const result = factorial(value);
+    resultbox.textContent = value + "! = " + result;
+    resultbox.style.color = "green";
+  }
+});
---
+++ mathModule.js
@@ -0,0 +1,10 @@
+function factorial(num) {
+  if (num < 0) return undifined;
+  if (num === 0 || num === 1) return 1;
+
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result = result * i;
+  }
+  return result;
+}
